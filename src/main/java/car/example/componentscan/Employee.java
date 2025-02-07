f5a7c10ba8a2ad64cfebbb3c4ca0ad3fe99ee2dd
Ramsai999 <ramsapuri@gmail.com>
2025-02-07 06:41:30 +0530

    Implemention of Beans and IoC (Inversion of control using Annotations (@component , @component scan @Value ) by configuring in xml file . Comments indicate how the acutal code is replaced by with simple annotations

---
+++ src/main/java/car/example/componentscan/Employee.java
@@ -0,0 +1,53 @@
+package car.example.componentscan;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Employee {
+    @Value("#{808}")
+    private int  employeeId;
+    @Value("Ram")
+    private String firstName;
+    @Value("s")
+    private String lastName;
+    @Value("#{999999}")
+    private double salary;
+
+//    public int getEmployeeId() {
+//        return employeeId;
+//    }
+//
+//    public void setEmployeeId(int employeeId) {
+//        this.employeeId = employeeId;
+//    }
+//    public String getFirstName() {
+//        return firstName;
+//    }
+//
+//    public void setFirstName(String firstName) {
+//        this.firstName = firstName;
+//    }
+//    public String getLastName() {
+//        return lastName;
+//    }
+//    public void setLastName(String lastName) {
+//        this.lastName = lastName;
+//    }
+//    public double getSalary() {
+//        return salary;
+//    }
+//    public void setSalary(double salary) {
+//        this.salary = salary;
+//    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employeeId=" + employeeId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
